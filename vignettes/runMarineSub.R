9903f2e3e077191efc7d2f624f42a1daeaf6609a
wynn <wynn@berkeley.edu>
2018-06-08 15:53:55 -0400

    Continuing to update binary vignette and runMarineSub
    Making binary walk-through more user-friendly and compatible with
    continuous vignette and up-to-date functions, by addressing the following
    issues:
    
    - making files accessible via R library paths
    - separating installation from run commands
    - eliminating text about mean-variance plot which no longer appears
    - updating which parameters need to be set for getAllResiduals
    - describing the heteroscedasticity plots that appear with getAllResiduals
    - referencing the runMarineSub script specifically in the binary trait section

--- vignettes/runMarineSub.R
+++ vignettes/runMarineSub.R
@@ -1,54 +1,49 @@
-if(T){
-  if (!require("RERconverge", character.only=T, quietly=T)) {
-    require(devtools)
-    install_github("nclark-lab/RERconverge")
-  }
-#the following will take some time
-mamTrees=readTrees("../data/mammal62aa_meredplus_wCM.trees", max.read = 200)
-#cutoff = exp(-7) based on the peak of the mean/variance plot
+
+library("RERconverge")
+rerpath = paste(.libPaths()[1],"/RERconverge",sep="")
+toytreefile = "mammal62aa_meredplus_wCM.trees" #change filename once toy trees available
+#this will take some time
+mamTrees=readTrees(paste(rerpath,"/extdata/",toytreefile,sep=""), max.read = 200)
 
 
-#this is the basic method (not demonstrated in the vignette)
-mamRER=getAllResiduals(mamTrees,useSpecies=mamTrees$masterTree$tip.label, transform = "none",weighted = F, scale = T)
-#For some datasets scaling improves results
-
-#this method peforms better on benchmarks, weighting works with all transforms but sqrt is recommended
-#(scaling no longer recommended and not demonstrated in the vignette)
-mamRERw=getAllResiduals(mamTrees,useSpecies=mamTrees$masterTree$tip.label, transform = "sqrt",weighted = T, scale=T)
-
-#this method peforms better on benchmarks, weighting works with all transforms but sqrt is recommended
-mamRERv=getAllResiduals(mamTrees,useSpecies=mamTrees$masterTree$tip.label,
-                       transform = "sqrt", weighted = T, cutoff=0.001)
+#this is the basic method (not used in the vignette)
+mamRER=getAllResiduals(mamTrees,useSpecies=mamTrees$masterTree$tip.label, 
+                       transform = "none",weighted = F, scale = T)
 
 
+#this method peforms better on benchmarks and is used in the vignette
+#weighting works with all transforms but sqrt is recommended
+mamRERw=getAllResiduals(mamTrees,useSpecies=mamTrees$masterTree$tip.label, 
+                        transform = "sqrt",weighted = T, scale=T)
 
 
 #read the binary tree
-marineb=read.tree("data/MarineTreeBin.txt")
-#offset 0 length edges, makes no difference for further computation
+marineb=read.tree(paste(rerpath,"/extdata/MarineTreeBin.txt",sep=""))
+#offset 0 length edges, makes no difference for further computation (double check this)
 marineb$edge.length[marineb$edge.length<1]=0.05
 plot(marineb)
 
-#we can also do this from the foreground set but only the extant branches will be set to 1
+#we can also do this from the foreground set
 foreground=c("triMan1", "orcOrc1", "turTru2", "odoRosDi", "lepWed1")
-plot(marineb)
+#there are two options, the default will put all the weight for a clade on the ancestral branch
+marineb1=foreground2Tree(foreground, mamTrees, collapse2anc = T)
+#alternatively the weight will be put on the extant branch but this approach suffers from phylogenetic dependence
+marineb2=foreground2Tree(foreground, mamTrees, collapse2anc = F)
+#another approach is to spread the weight along the entire clade, like this. In order to handle the phylogenetic dependence in this case we case use weighted correlation, see bellow
+marineb3=foreground2Tree(foreground, mamTrees, wholeClade = T)
 
-marineb2=foreground2Tree(foreground, mamTrees)
-marineb3=foreground2Tree(foreground, mamTrees, collapse2anc = F)
 
 
 #convert a marine tree to a paths vector
-phenvMarine=tree2Paths(marineb, mamTrees)
-phenvMarine
+phenvMarine=tree2Paths(marineb1, mamTrees)
+#do the correlation
+corMarine1=getAllCor(mamRERw, phenvMarine)
 
-corMarine = getAllCor(mamRERv, phenvMarine)
+#we can also do this with the other trees we generated
+corMarine2=getAllCor(mamRERw, tree2Paths(marineb2, mamTrees))
+
+#If we want to consider the whole clade as a single observation (using tree version 3) we need to use weighted correlation
+corMarine3=getAllCor(mamRERw, tree2Paths(marineb3, mamTrees), weights = T)
 
 
-#corMarine=getAllCor(mamRER, phenvMarine)
-#hist(corMarine$P)
 
-#corMarineW=getAllCor(mamRERw, phenvMarine)
-#hist(corMarineW$P)
-#qqplot(corMarine$P, corMarineW$P, log="xy");abline(a=0,b=1)
-
-}
