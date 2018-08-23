caafca6e85c87e69a4a0c45ee968635a009663fd
sorrywm <wynnmeyer@mc-wifi-10-215-143-18.wireless.pitt.edu>
2018-08-23 12:31:19 -0400

    Adding new test trees and revising vignette
    For testing function edits:
    -added a tree with more species than the demo master tree
    -began revising a script to perform checks on discoradant trees
    
    For the vignette:
    -incorporated find.package() to locate RERconverge files
    -knit to html and pdf

--- tests/checkTreeMatch.R
+++ tests/checkTreeMatch.R
@@ -1,6 +1,17 @@
 #Check that RERconverge will identify issues with tree topology,
 #namely, ones where the trait tree is not a subset of the master gene tree.
+library('RERconverge')
+rerpath = find.package('RERconverge')
+
+#Use same master tree for all cases:
+toytreefile = "subsetMammalGeneTrees.txt" 
+toyTrees=readTrees(paste(rerpath,"/extdata/",toytreefile,sep=""), max.read = 200)
+
 #Example cases:
 # -rooted trait tree
 # -different tree topology
+# -species in the trait tree that are not in the master tree
+pon1tree = read.tree(paste(rerpath,'PON1_addlspecies.tre',sep='/'))
+ttpon1 = tree2Paths(pon1tree, toyTrees)
+
 # -different species filtered from trait tree and master tree
